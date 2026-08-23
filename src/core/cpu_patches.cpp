d2368909fa6ed89e4c5e92a82ce563608b476d7e
yellowsparkles584 <154008927+yellowsparkles584@users.noreply.github.com>
2026-08-23 20:30:35 +1000

    •

--- src/core/cpu_patches.cpp
+++ src/core/cpu_patches.cpp
@@ -800,7 +800,7 @@ static bool TryExecuteIllegalInstruction(void* ctx, void* code_address) {
         return false;
     }
 
-    ASSERT(bytes[1] == 0x0F && bytes[2] == 0x79);
+    ASSERT(bytes[1] == 0x0F && (bytes[2] == 0x79 || bytes[2] == 0x01));
 
     // Note: It's guaranteed that there's no REX prefix in these instructions checked by
     // Is4ByteExtrqOrInsertq
